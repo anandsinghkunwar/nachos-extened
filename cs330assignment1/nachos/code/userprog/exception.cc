d62f3eb578cebbddc4fad188e9c98127d271e36f
Anand Singh Kunwar <anandkunwar95@gmail.com>
2015-09-22 15:15:13 +0530

    Added two syscalls yield and getpa

--- cs330assignment1/nachos/code/userprog/exception.cc
+++ cs330assignment1/nachos/code/userprog/exception.cc
@@ -26,7 +26,7 @@
 #include "syscall.h"
 #include "console.h"
 #include "synch.h"
-
+#include "addrspace.h"
 //----------------------------------------------------------------------
 // ExceptionHandler
 // 	Entry point into the Nachos kernel.  Called when a user program
@@ -173,6 +173,37 @@ ExceptionHandler(ExceptionType which)
        machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
        machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
     }
+    else if ((which == SyscallException) && (type == syscall_GetPA)){
+      vaddr = machine->ReadRegister(4);
+      unsigned int pageFrame,offset,vpn = vaddr/PageSize;
+      if(vpn >= machine->pageTableSize ){   //virtual page number is larger than the number of entries in the page table
+       machine->WriteRegister(2,-1);
+      }
+      else if (!machine->pageTable[vpn].valid){   //The page table entry has the valid field set to false
+       machine->WriteRegister(2,-1);
+      }
+      TranslationEntry *entry;
+      entry = &machine->pageTable[vpn];
+      pageFrame = entry->physicalPage;
+      offset = (unsigned) vaddr % PageSize;
+      if (pageFrame >= NumPhysPages){   //physical page number is larger than the # of physical pages
+       machine->WriteRegister(2,-1);
+      }
+      else{   //return physical address
+       machine->WriteRegister(2,(pageFrame * PageSize)+offset);
+      }
+       // Advance program counters.
+       machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
+       machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
+       machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
+    }
+    else if ((which == SyscallException) && (type == syscall_Yield)){
+       currentThread->YieldCPU();
+       // Advance program counters.
+       machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
+       machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
+       machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
+  }
     else {
 	printf("Unexpected user mode exception %d %d\n", which, type);
 	ASSERT(FALSE);
