15e15ef53449f7c408155067eea9e1eb2bdce5da
Aadi Yogakar <ayogakar@iitk.ac.in>
2015-09-25 17:08:01 +0530

    modified syscall_NumInstr

--- cs330assignment1/nachos/code/test/testNumInstr.c
+++ cs330assignment1/nachos/code/test/testNumInstr.c
@@ -3,25 +3,10 @@
 
 int main ()
 {
-	    // int array[SIZE], i, sum=0;
-
-    // system_PrintString("Starting physical address of array: ");
-    // system_PrintInt(system_GetPA((unsigned)array));
-    // system_PrintChar('\n');
+    int a;
     system_PrintString("I am printing: ");
-    system_PrintInt(system_GetNumInstr());
+    a = system_GetNumInstr();
+    system_PrintInt(a);
     system_PrintChar('\n');
-    // system_PrintString("Current physical address of stack top: ");
-    // system_PrintInt(system_GetPA(system_GetReg(29)));
-    // system_PrintChar('\n');
-    // for (i=0; i<SIZE; i++) array[i] = i;
-    // system_PrintString("We are currently at PC: ");
-    // system_PrintIntHex(system_GetReg(34));
-    // system_PrintChar('\n');
-    // for (i=0; i<SIZE; i++) sum += array[i];
-    // system_PrintString("Total sum: ");
-    // system_PrintInt(sum);
-    // system_PrintChar('\n');
-    // system_Exit(0);
     return 0;
 }
--- cs330assignment1/nachos/code/userprog/exception.cc
+++ cs330assignment1/nachos/code/userprog/exception.cc
@@ -219,7 +219,7 @@ ExceptionHandler(ExceptionType which)
        machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
     }
     else if ((which == SyscallException) && (type == syscall_NumInstr)) {
-       machine->WriteRegister(2, currentThread->getNumInstr());
+       machine->WriteRegister(2, currentThread->getNumInstr()-5);
        // Advance program counters.
        machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
        machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
