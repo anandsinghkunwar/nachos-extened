959c8297006de6fd9b93a4ba17eae451da905bcf
Anand Singh Kunwar <anandkunwar95@gmail.com>
2015-09-27 18:36:52 +0530

    Removed -5 offset from getNumInstr. The getter reports correct value for getNumInstr.

--- cs330assignment1/nachos/code/userprog/exception.cc
+++ cs330assignment1/nachos/code/userprog/exception.cc
@@ -237,7 +237,7 @@ ExceptionHandler(ExceptionType which)
        machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
     }
     else if ((which == SyscallException) && (type == syscall_NumInstr)) {
-       machine->WriteRegister(2, currentThread->getNumInstr()-5);
+       machine->WriteRegister(2, currentThread->getNumInstr());
        // Advance program counters.
        machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
        machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
